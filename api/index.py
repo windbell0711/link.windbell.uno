a0bb8f288f256df4855bd8641dcb7119c302403a
windbell0711 <windbell07@outlook.com>
2026-07-30 20:50:43 +0800

    fix

--- api/index.py
+++ api/index.py
@@ -1,9 +1,8 @@
 ﻿import dotenv
 from os import getenv 
-from datetime import datetime, timezone, date
+from datetime import datetime, timezone
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 import asyncpg
 
@@ -22,13 +21,14 @@ async def lifespan(app: FastAPI):
     
     yield
     
+    await app.state.pool.close()
     print("Database pool closed.")
 
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["http://127.0.0.1:5500"],  # 开发环境
     allow_methods=["GET"],
     allow_credentials=True,
 )
@@ -36,16 +36,17 @@ app.add_middleware(
 @app.get("/api/redirect")
 async def redirect(code: str):
     pool: asyncpg.pool.Pool = app.state.pool
+
     async with pool.acquire() as conn:
         row = await conn.fetchrow(
             "SELECT * FROM link WHERE code = $1", code
         )
         if not row:
-            return HTMLResponse("<h2>code不存在</h2>", status_code=404)
+            return {"valid": False, "msg": "code不存在"}
         if row["date_begin"] and row["date_begin"] > datetime.now(timezone.utc):
-            return HTMLResponse("<h2>链接未生效</h2>", status_code=403)
+            return {"valid": False, "msg": "链接未生效"}
         if row["date_end"] and row["date_end"] < datetime.now(timezone.utc):
-            return HTMLResponse("<h2>链接已过期</h2>", status_code=410)
+            return {"valid": False, "msg": "链接已过期"}
         today = datetime.now(timezone.utc).date()
         last_access = row["access_lastdate"]
         if isinstance(last_access, datetime):
@@ -54,12 +55,12 @@ async def redirect(code: str):
             same_day = last_access == today
         if same_day:
             if row["access_daylmt"] > 0 and row["access_daycnt"] >= row["access_daylmt"]:
-                return HTMLResponse("<h2>链接访问次数已达上限</h2>", status_code=429)
+                return {"valid": False, "msg": "链接访问次数已达上限"}
             await conn.execute(
                 "UPDATE link SET access_daycnt = access_daycnt + 1 WHERE code = $1", code
             )
         else:
             await conn.execute(
-                "UPDATE link SET access_daycnt = 1, access_lastdate = $2 WHERE code = $1", code, today
+                "UPDATE link SET access_daycnt = 1, access_lastdate = $2 WHERE code = $1", code, datetime.now(timezone.utc).date()
             )
-        return RedirectResponse(url=row["url"])
+        return {"valid": True, "url": row["url"]}
