801e97c44f128a554912d00dd6d5971c73cd9e69
windbell0711 <windbell07@outlook.com>
2026-08-02 14:20:00 +0800

    feat: oss

--- api/index.py
+++ api/index.py
@@ -1,13 +1,27 @@
 ﻿import dotenv
 from os import getenv 
-from datetime import datetime, timezone
+from datetime import datetime, date, timezone
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import asyncpg
 
 dotenv.load_dotenv()
 assert getenv("DATABASE_URL"), "DATABASE_URL environment variable not set."
+
+today = lambda: datetime.now(timezone.utc).date()
+
+class PgLink(BaseModel):
+    id: int
+    code: str
+    url: str
+    access_lastdate: date
+    access_daylmt: int = 0
+    access_daycnt: int = 5
+    is_oss: bool = False
+    date_begin: date | None = None
+    date_end: date | None = None
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -36,31 +50,36 @@ app.add_middleware(
 @app.get("/api/redirect")
 async def redirect(code: str):
     pool: asyncpg.pool.Pool = app.state.pool
-
     async with pool.acquire() as conn:
+        # 查询数据库
         row = await conn.fetchrow(
             "SELECT * FROM link WHERE code = $1", code
         )
         if not row:
             return {"valid": False, "msg": "code不存在"}
-        if row["date_begin"] and row["date_begin"] > datetime.now(timezone.utc):
+        # 校验链接是否有效
+        link = PgLink.model_validate(dict(row))
+        if link.date_begin and link.date_begin > today():
             return {"valid": False, "msg": "链接未生效"}
-        if row["date_end"] and row["date_end"] < datetime.now(timezone.utc):
+        if link.date_end and link.date_end < today():
             return {"valid": False, "msg": "链接已过期"}
-        today = datetime.now(timezone.utc).date()
-        last_access = row["access_lastdate"]
-        if isinstance(last_access, datetime):
-            same_day = last_access.date() == today
-        else:
-            same_day = last_access == today
-        if same_day:
-            if row["access_daylmt"] > 0 and row["access_daycnt"] >= row["access_daylmt"]:
+        # 校验链接是否次数已达上限，更新访问次数
+        if link.access_lastdate == today():
+            if link.access_daylmt > 0 and link.access_daycnt >= link.access_daylmt:
                 return {"valid": False, "msg": "链接访问次数已达上限"}
             await conn.execute(
                 "UPDATE link SET access_daycnt = access_daycnt + 1 WHERE code = $1", code
             )
         else:
             await conn.execute(
-                "UPDATE link SET access_daycnt = 1, access_lastdate = $2 WHERE code = $1", code, datetime.now(timezone.utc).date()
+                "UPDATE link SET access_daycnt = 1, access_lastdate = $2 WHERE code = $1", code, today()
             )
-        return {"valid": True, "url": row["url"]}
+        # 特判是否为oss链接，生成预签名临时链接返回给前端重定向
+        ret_url = link.url
+        if link.is_oss:
+            import oss
+            ret_url = oss.get_oss_url("wint-storage-1", object_key=link.url)
+            if not ret_url:
+                return {"valid": False, "msg": "oss链接生成失败"}
+        # 返回重定向链接
+        return {"valid": True, "url": ret_url}
---
+++ api/oss.py
@@ -0,0 +1,21 @@
+﻿from datetime import timedelta
+import alibabacloud_oss_v2 as oss2
+
+# 1. 从环境变量读取密钥
+credentials_provider = oss2.credentials.EnvironmentVariableCredentialsProvider()
+
+# 2. 配置客户端
+cfg = oss2.config.load_default()
+cfg.credentials_provider = credentials_provider
+cfg.region = 'cn-hongkong'
+
+# 3. 创建客户端
+client = oss2.Client(cfg)
+
+# 4. 生成 OSS 预签名临时链接
+def get_oss_url(bucket_name: str, object_key: str) -> str | None:
+    result = client.presign(
+        oss2.models.GetObjectRequest(bucket=bucket_name, key=object_key),
+        expires=timedelta(minutes=30),
+    )
+    return result.url
