e08987be6fc4528dd3ebaf2e26185183f87de309
windbell0711 <windbell07@outlook.com>
2026-08-02 19:23:35 +0800

    fix

--- api/oss.py
+++ api/oss.py
@@ -1,9 +1,12 @@
 ﻿import dotenv
+from os import getenv
 from datetime import timedelta
 import alibabacloud_oss_v2 as oss2
 
 # 1. 从环境变量读取密钥
 dotenv.load_dotenv()
+assert getenv("OSS_ACCESS_KEY_ID"), "OSS_ACCESS_KEY_ID environment variable not set."
+assert getenv("OSS_ACCESS_KEY_SECRET"), "OSS_ACCESS_KEY_SECRET environment variable not set."
 credentials_provider = oss2.credentials.EnvironmentVariableCredentialsProvider()
 
 # 2. 配置客户端
